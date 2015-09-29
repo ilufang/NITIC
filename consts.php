72ce882d88ad48450727d42e46b677445ed44bcc
ilufang <cc2lufang@gmail.com>
2015-09-29 10:05:04 +0800

    upload

---
+++ consts.php
@@ -0,0 +1,16 @@
+<?php
+/*
+	consts.php
+	This file saves some constant values for easier configuration.
+*/
+
+// MySQL database login credentials
+$db_server = "localhost";	// Server address
+$db_user = "root";			// DB Username
+$db_pswd = "mysql";			// DB Password
+$db_name = "nitic";			// Name for DB
+
+
+// Initial admin account info
+$admin_user = "root";	// Admin Username
+$admin_pswd = "";		// Admin Password
